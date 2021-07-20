082ca53c7cc8fcdc22555d0ccd695e67a966525e
Emily Simonton <simonton.16@buckeyemail.osu.edu>
2021-07-20 14:55:05 +0000

    lmdata added

--- shiny_lmdata_final/app.R
+++ shiny_lmdata_final/app.R
@@ -7,15 +7,6 @@
 #    http://shiny.rstudio.com/
 #
 
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 
